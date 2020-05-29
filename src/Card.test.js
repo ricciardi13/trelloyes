c43f07b325cd9e50c797c36dce39caf8eec84f58
Nathan Ricciardi <nathan.ricciardi@gmail.com>
2020-05-29 13:03:06 -0700

    Creates component tests

---
+++ src/Card.test.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import renderer from "react-test-renderer";
+import Card from "./Card";
+
+describe("Card component", () => {
+    it("renders without crashing", () => {
+        const div = document.createElement("div");
+        ReactDOM.render(<Card />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
+    it("renders the card as expected", () => {
+        const tree = renderer
+            .create(<Card />)
+            .toJSON();
+        expect(tree).toMatchSnapshot();    
+    });
+});
